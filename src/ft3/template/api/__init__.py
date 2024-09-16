5c25a5cf81f4b2cdcaa65c3e10a59a94ab74302e
dan1hc <dan@1howardcapital.com>
2024-09-16 12:11:56 -0700

    fix: template api str locations

--- src/ft3/template/api/__init__.py
+++ src/ft3/template/api/__init__.py
@@ -4,9 +4,6 @@ Summary
 
 A simple python API that can be copy / pasted / replaced.
 
-_This description was sourced from the docstring of the root \
-level `__init__.py` file of the `ft3.template` subpackage._
-
 """
 
 __all__ = (
